95366e0614166e199506619ca5f67f316bde9636
Ahmed Ikram <ahmed@Ahmeds-MacBook-Pro.local>
2025-06-26 20:34:36 +0500

    feat: added users routes and service methods

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly userService: UsersService,
+  ) {}
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id });
+  }
+
+  @Get('')
+  async getUsers(
+    @Query('skip') skip?: string,
+    @Query('take') take?: string,
+    @Query('cursor') cursor?: string,
+    @Query('orderBy') orderBy?: string,
+  ): Promise<UserModel[]> {
+    return this.userService.users({
+      skip: skip ? parseInt(skip) : undefined,
+      take: take ? parseInt(take) : undefined,
+      cursor: cursor ? { id: cursor } : undefined,
+      orderBy: orderBy ? { id: orderBy as 'asc' | 'desc' } : undefined,
+    });
+  }
+
+  @Post('')
+  async createUser(@Body() data: Prisma.UserCreateInput): Promise<UserModel> {
+    return this.userService.createUser(data);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: Prisma.UserUpdateInput,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id },
+      data,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id });
+  }
+}
